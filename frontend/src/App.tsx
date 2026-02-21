8d27453e99f39c85d29deac2f421c7f15e54de2a
hdpontes <150452771+hdpontes@users.noreply.github.com>
2026-02-21 20:03:47 -0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import DashboardPage from './pages/DashboardPage';
 import TrucksPage from './pages/TrucksPage';
 import TruckDetailPage from './pages/TruckDetailPage';
 import TripsPage from './pages/TripsPage';
+import TripFormPage from './pages/TripFormPage';
 import ExpensesPage from './pages/ExpensesPage';
 import MaintenancePage from './pages/MaintenancePage';
 import DriversPage from './pages/DriversPage';
@@ -32,6 +33,7 @@ function App() {
           <Route path="trucks" element={<TrucksPage />} />
           <Route path="trucks/:id" element={<TruckDetailPage />} />
           <Route path="trips" element={<TripsPage />} />
+          <Route path="trips/new" element={<TripFormPage />} />
           <Route path="expenses" element={<ExpensesPage />} />
           <Route path="maintenance" element={<MaintenancePage />} />
           <Route path="drivers" element={<DriversPage />} />
