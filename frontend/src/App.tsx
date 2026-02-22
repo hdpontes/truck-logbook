7662d1e6c6591c498e8f8307e5fe5ba5d3eb42c7
hdpontes <150452771+hdpontes@users.noreply.github.com>
2026-02-21 21:37:54 -0300

    feat: adicionar registro de despesas para motoristas e filtrar viagens por usuario logado

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ import TruckFormPage from './pages/TruckFormPage';
 import TripsPage from './pages/TripsPage';
 import TripFormPage from './pages/TripFormPage';
 import ExpensesPage from './pages/ExpensesPage';
+import ExpenseFormPage from './pages/ExpenseFormPage';
 import MaintenancePage from './pages/MaintenancePage';
 import DriversPage from './pages/DriversPage';
 import UsersPage from './pages/UsersPage';
@@ -41,6 +42,7 @@ function App() {
           <Route path="trips" element={<TripsPage />} />
           <Route path="trips/new" element={<TripFormPage />} />
           <Route path="expenses" element={<ExpensesPage />} />
+          <Route path="expenses/new" element={<ExpenseFormPage />} />
           <Route path="maintenance" element={<MaintenancePage />} />
           <Route path="drivers" element={<DriversPage />} />
           <Route path="users" element={<UsersPage />} />
