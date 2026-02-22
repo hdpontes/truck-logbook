5eda236a50d2973de05c71aa82a52a6e0d0db5c0
hdpontes <150452771+hdpontes@users.noreply.github.com>
2026-02-22 14:48:33 -0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,6 +15,7 @@ import DriversPage from './pages/DriversPage';
 import UsersPage from './pages/UsersPage';
 import ClientsPage from './pages/ClientsPage';
 import LocationsPage from './pages/LocationsPage';
+import SettingsPage from './pages/SettingsPage';
 import Layout from './components/Layout';
 
 function PrivateRoute({ children }: { children: React.ReactNode }) {
@@ -70,6 +71,7 @@ function App() {
           <Route path="users" element={<RoleRoute allowedRoles={['ADMIN']}><UsersPage /></RoleRoute>} />
           <Route path="clients" element={<RoleRoute allowedRoles={['ADMIN', 'MANAGER']}><ClientsPage /></RoleRoute>} />
           <Route path="locations" element={<RoleRoute allowedRoles={['ADMIN', 'MANAGER']}><LocationsPage /></RoleRoute>} />
+          <Route path="settings" element={<RoleRoute allowedRoles={['ADMIN', 'MANAGER']}><SettingsPage /></RoleRoute>} />
         </Route>
       </Routes>
     </BrowserRouter>
